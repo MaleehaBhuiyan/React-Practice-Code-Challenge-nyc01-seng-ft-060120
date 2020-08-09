983c6b8d47cb1aab8daee3667926b0b11b664fbe
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-08-09 12:13:18 -0400

    first three deliverables

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,10 +7,34 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    spliceStart: 0,
+    spliceEnd: 4,
+    sushiArray: []
+  }
+
+  componentDidMount(){
+    fetch(`${API}`)
+    .then(response => response.json())
+    .then(sushiData => this.setState({ sushiArray:sushiData }))
+  }
+
+  chooseFourSushis = () => {
+    return this.state.sushiArray.slice(this.state.spliceStart, this.state.spliceEnd)
+  }
+
+  moreButton = () => {
+
+    let newSpliceStart = this.state.spliceStart + 4 
+    let newSpliceEnd = this.state.spliceEnd + 4 
+    this.setState({ spliceStart: newSpliceStart, spliceEnd: newSpliceEnd})
+    // this.setState({ sushiArray: this.state.sushiArray.splice(this.state.spliceStart, this.state.spliceEnd) })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
+        <SushiContainer sushiArray={this.chooseFourSushis()} moreButton={this.moreButton} />
         <Table />
       </div>
     );
@@ -18,3 +42,4 @@ class App extends Component {
 }
 
 export default App;
+
